cc5249bc5b48579fba86f6d3728d24177ba6c7d5
Akshithaaashi <akshitha.aashi@gmail.com>
2021-06-30 01:00:10 +0530

    Added : extended the max method of morethan 3 parameters

--- src/com/generics/genericsProblems.java
+++ src/com/generics/genericsProblems.java
@@ -1,24 +1,28 @@
 package com.generics;
+public class genericsProblems <T extends Comparable>{
+    private T[] array;
 
-public class genericsProblems {
-public static void main(String[] args) {
-			
-		String l= "apple", m = "banana", n = "carrot"; 
-	    
-	    findMaximum(l,m,n);
-	}
-	 
-			private static void findMaximum(String l, String m, String n) {
-				String max=l;
-				if(m.compareTo(l) > 0) {
-					max = m;
-				}
-				if(n.compareTo(max) > 0) {
-					max = n;
-					System.out.println(max);
-	          }
-			}
+    public genericsProblems(T[] array) {
+        this.array = array;
+    }
+
+    public static void main(String[] args) {
+        Integer[] intArray={5,15,96,53,45,74};
+        Double[] doubleArray={5.6,4.2,3.6,8.4,9.4,4.2};
+        String[] stringArray={"cat","dog","lion","tiger","elephant","zebra"};
+        genericsProblems.getMaximum(intArray);
+        genericsProblems.getMaximum(doubleArray);
+        genericsProblems.getMaximum(stringArray);
+    }
+    private static <T extends Comparable> void getMaximum(T[] array){
+
+        T max = array[0];
+        for (int i=0;i<array.length; i++){
+            if (array[i].compareTo(max)>0){
+                max=array[i];
+            }
+        }
+        System.out.println("Maximum : "+max);
+
+    }
 }
-
-
-
