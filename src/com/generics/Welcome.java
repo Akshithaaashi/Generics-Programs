7e7ecaf5e37fcce88b51d0f1c4841841d6a9938b
Akshithaaashi <akshitha.aashi@gmail.com>
2021-06-30 00:26:37 +0530

    Added : To find max of 3 Integers

--- src/com/generics/Welcome.java
+++
@@ -1,7 +0,0 @@
-package com.generics;
-
-public class Welcome {
-  public static void main(String[] args) {
-	  System.out.println("Welcome");
-}
-}
---
+++ src/com/generics/maxOf3Integers.java
@@ -0,0 +1,23 @@
+package com.generics;
+
+public class maxOf3Integers {
+public static void main(String[] args) {
+		
+		Integer a=2, b=4, c=6;
+	    findMaximum(a,b,c);
+	    
+	}
+	 
+	private static void findMaximum(Integer a, Integer b, Integer c) {
+		Integer max=a;
+		if(b.compareTo(a) > 0) {
+			max = b;
+		}
+		if(c.compareTo(max) > 0) {
+			max = c;
+			System.out.println(max);
+	}
+	
+  }
+
+}
